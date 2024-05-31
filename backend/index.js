e578dea793c5fc93ecf15fb1eb2f0a69098fbd5a
CryoWarlocK <karunarathna_js_e23@engug.ruh.ac.lk>
2024-05-31 21:54:26 +0530

    finalize the backend with cors, and also express routes

--- backend/index.js
+++ backend/index.js
@@ -2,138 +2,36 @@ import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
 import { Book } from "./models/bookModel.js"
+import bookRoute from "./routes/bookRoute.js"
 
 const app = express();
 
 //middleware for parsing request body 
 app.use(express.json());
 
-
-
-app.post('/books', async (req, res) => {
-    try {
-        if (
-            !req.body.title || !req.body.author || !req.body.publishYear
-        ) {
-            return res.status(400).send({
-                message: "Send all required fields: title, author, publishYear",
-            })
-        }
-
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear,
-        }
-
-        const book = await Book.create(newBook);
-        return res.status(201).send(book);
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            message: error.message,
-        })
-
-    }
-})
-
-
-//get all the books from database
-app.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find({});
-        return res.status(200).json({
-            count: books.length,
-            data: books
-        });
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            message: error.message,
-        })
-    }
-})
-
-//get a specific book from database
-app.get('/books/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const book = await Book.findById(id);
-        return res.status(200).json(book)
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            message: error.message,
-        })
-    }
-})
-
-//update a book
-app.put('/books/:id', async (req, res) => {
-    try {
-        //validation
-        if (
-            !req.body.title || !req.body.author || !req.body.publishYear
-        ) {
-            return res.status(400).send({
-                message: "Send all required fields: title, author, publishYear",
-            })
-        }
-
-        const { id } = req.params;
-        const result = await Book.findByIdAndUpdate(id, req.body);
-
-        if (!result) {
-            return res.status(404).send({
-                message: "Book not found",
-            })
-        }
-
-        return res.status(200).send({ message: 'Book updated succesfully' })
-
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            message: error.message,
-        })
-
-    }
-})
-
-// delete a book
-app.delete('/books/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const result = await Book.findByIdAndDelete(id);
-        if (!result) {
-            return res.status(404).send({
-                message: "Book not found",
-            })
-        }
-        return res.status(200).send({ message: 'Book deleted succesfully' })
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            message: error.message,
-        })
-
-    }
-})
-
+//middleware for handling cors policy
+//option 1 : allow all origins with default of cors(*) 
+//app.use(cors())
+// option 2 : allow custom origins
+app.use(cors({
+    origin: 'http:localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-type'],
+}))
 
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(200).send('Welcome to MERN stack');
 });
+
+
+app.use('/books', bookRoute)
+
+
+
+
+
+
 
 const startServer = async () => {
     try {
